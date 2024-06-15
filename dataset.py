811def635a4b64a174d55c7e1ac2f09bc08e0511
AsphodelRem <asphodel.bog.rem@gmail.com>
2024-06-15 18:29:45 +0700

    Added dataset for faster RCNN

--- dataset.py
+++ dataset.py
@@ -2,10 +2,13 @@ import os
 import torch
 import torch.nn as nn
 import torchvision
+from torch.utils.data import DataLoader, Dataset
 import pandas as pd
 import numpy as np
 import json
 from PIL import Image
+from augmentations import augment
+from torchvision.transforms import functional as F
 
 import convert_to_coco as ctc
 
@@ -42,6 +45,9 @@ class AtomicDataset(torchvision.datasets.CocoDetection):
             )
         ).convert('RGB')
 
+        if self.train:
+            img = augment(np.array(img))
+
         target = {'image_id': image_id, 'annotations': [target]}
 
         encoding = self.processor(images=img, annotations=target, return_tensors="pt")
@@ -72,9 +78,79 @@ class AtomicDataset(torchvision.datasets.CocoDetection):
         )
 
     @staticmethod
-    def collate_fn(batch):
+    def collate_fn(self, batch):
         pixel_values = [item[0] for item in batch]
         encoding = self.processor.pad(pixel_values, return_tensors="pt")
         labels = [item[1] for item in batch]
-        batch = {'pixel_values': encoding['pixel_values'], 'pixel_mask': encoding['pixel_mask'], 'labels': labels}
+        batch = {
+            'pixel_values': encoding['pixel_values'],
+            'pixel_mask': encoding['pixel_mask'],
+            'labels': labels
+        }
         return batch
+
+
+class AtomicDatasetV2(Dataset):
+    def __init__(self, csv_file, root_dir, config, transforms=None):
+        self.annotations = pd.read_csv(csv_file).dropna()
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.config = config
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[idx, 1])
+        img = Image.open(img_path).convert('RGB')
+
+        boxes = []
+        labels = []
+
+        label = self.annotations.iloc[idx, 2]
+        rel_x = self.annotations.iloc[idx, 3]
+        rel_y = self.annotations.iloc[idx, 4]
+        height = self.annotations.iloc[idx, 5]
+        width = self.annotations.iloc[idx, 6]
+
+        w, h = img.size
+        xmin = rel_x * w
+        ymin = rel_y * h
+        xmax = xmin + width * w
+        ymax = ymin + height * h
+
+        boxes.append([xmin, ymin, xmax, ymax])
+        labels.append(label)
+
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        iscrowd = torch.zeros((len(boxes),), dtype=torch.int64)
+
+        target = {
+            "boxes": boxes,
+            "labels": labels,
+            "image_id": image_id,
+            "area": area,
+            "iscrowd": iscrowd
+        }
+
+        img = F.to_tensor(img)
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.annotations)
+
+    @staticmethod
+    def create_train_test_split(config: dict) -> None:
+        metadata = pd.read_csv(config['metadata_parameters']['path_to_unsplitted_metadata']).dropna()
+
+        split_index = int(len(metadata) * config['metadata_parameters']['split_ratio'])
+
+        # Split on train and test
+        train_metadata = metadata[:split_index]
+        test_metadata = metadata[split_index:]
+
+        train_metadata.to_csv(config['metadata_parameters']['path_to_train_metadata'])
+        test_metadata.to_csv(config['metadata_parameters']['path_to_test_metadata'])
