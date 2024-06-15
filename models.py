5ef04b5230830ea1e35e5327252589073c85f557
AsphodelRem <asphodel.bog.rem@gmail.com>
2024-06-15 18:29:55 +0700

    Added faster RCNN

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from transformers import AutoImageProcessor, AutoModelForObjectDetection, AutoFeatureExtractor
 import lightning as L
 import torch
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+import torchvision
 
 
 class AtomicModelWrapper(L.LightningModule):
@@ -17,6 +19,7 @@ class AtomicModelWrapper(L.LightningModule):
         self.config = config
 
         torch.set_float32_matmul_precision('medium')
+        self.metric = MeanAveragePrecision()
 
     def forward(self, pixel_values, pixel_mask):
         return self.model(
@@ -41,10 +44,10 @@ class AtomicModelWrapper(L.LightningModule):
         loss = outputs.loss
         loss_dict = outputs.loss_dict
 
-        return loss, loss_dict
+        return loss, loss_dict, outputs
 
     def training_step(self, batch, batch_idx):
-        loss, loss_dict = self.step(batch, batch_idx)
+        loss, loss_dict, out = self.step(batch, batch_idx)
         self.log('training_loss', loss, prog_bar=True)
 
         for k, v in loss_dict.items():
@@ -52,7 +55,8 @@ class AtomicModelWrapper(L.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, loss_dict = self.step(batch, batch_idx)
+        loss, loss_dict, out = self.step(batch, batch_idx)
+
         self.log('validation_loss', loss, prog_bar=True)
         for k, v in loss_dict.items():
             self.log('validation_' + k, v.item(), prog_bar=True)
@@ -81,3 +85,43 @@ class AtomicModelWrapper(L.LightningModule):
 
     def get_processor(self):
         return self.processor
+
+
+class AtomicFasterRCNN(L.LightningModule):
+    def __init__(self, config, num_classes):
+        super(AtomicFasterRCNN, self).__init__()
+        self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+            pretrained=True
+        )
+        in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        self.model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features,
+                                                                                                        num_classes)
+        self.config = config
+
+    def forward(self, images, targets=None):
+        return self.model(images, targets)
+
+    def training_step(self, batch, batch_idx):
+        images, targets = batch
+        loss_dict = self.model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+        self.log('train_loss', losses, prog_bar=True)
+        return losses
+
+    # def validation_step(self, batch, batch_idx):
+    #     images, targets = batch
+    #     loss_dict = self.model(images, targets)
+    #     print(loss_dict)
+    #     losses = sum(loss for loss in loss_dict.values())
+    #     self.log('val_loss', losses)
+    #     return losses
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            self.model.parameters(),
+            lr=self.config['optimizer_parameters']['learning_rate'],
+            weight_decay=self.config['optimizer_parameters']['weight_decay'],
+            fused=True
+        )
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        return [optimizer], [lr_scheduler]
